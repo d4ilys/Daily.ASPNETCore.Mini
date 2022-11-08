094fb8a085bd9ad1e55fa665e0a68a5f23c0d4f5
dailyccc <963922242@qq.com>
2022-11-08 11:02:32 +0800

    optimization

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Daily.ASPNETCore.Mini
- ASP.NET Core Mini 版本，仅用于学习..
-
-本项目可直接运行
+ ASP.NET Core Mini 版本，仅用于学习研究
 
 采用DotNetty处理HTTP
 
 HTTP管道、过滤器等均有实现
+
+本项目可直接运行-将Test设置为启动项
+
+http://127.0.0.1:8614/api/Home/Hello
+
---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Daily.ASPNETCore.Mini.MVC.ControllersRule;
+
+namespace Test.Controllers
+{
+
+    [DynamicController]
+    public class HomeController
+    {
+        public string Hello()
+        {
+            return "Hello Mini ASP.NET Core ";
+        }
+    }
+}
